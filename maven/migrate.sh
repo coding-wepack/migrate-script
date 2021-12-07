48cb09ffb9298a44c92939f8d79c0de24aab462e
chenxinyu <chenxinyu@coding.net>
2021-12-07 15:04:34 +0800

    Update maven migrate.sh

--- maven/migrate.sh
+++ maven/migrate.sh
@@ -2,15 +2,27 @@
 
 set -eo pipefail
 
-REPOSITORY_PATH=${REPOSITORY_PATH:-/Users/chenxinzhou/.m2/repository}
-REMOTE_REPO_URL=${REMOTE_REPO_URL:-http://127.0.0.1:8081/content/repositories/releases/}
-USERNAME=${USERNAME:-admin}
-PASSWORD=${PASSWORD:-coding123}
+# REPOSITORY_PATH 是 Maven Repository 的文件目录，通常是 $HOME/.m2/repository
+REPOSITORY_PATH=${REPOSITORY_PATH:-"/home/user/.m2/repository"}
+# REMOTE_REPO_URL 是 CODING Maven 仓库的 URL，通常格式为: http(s)://<team>-maven.pkg.<domain>/repository/<project>/<repository>
+# 需格外注意这里的 URL 结尾是没有斜线的，否则就会报 301.
+REMOTE_REPO_URL=${REMOTE_REPO_URL:-"https://demo-maven.pkg.coding.net/repository/test-project/maven-repository"}
+CODING_USERNAME=${CODING_USERNAME:-"test"}
+CODING_PASSWORD=${CODING_PASSWORD:-"test123"}
+
+echo "REPOSITORY_PATH: [$REPOSITORY_PATH]"
+echo "REMOTE_REPO_URL: [$REMOTE_REPO_URL]"
+echo "CODING_USERNAME: [$CODING_USERNAME]"
+echo "CODING_PASSWORD: [$CODING_PASSWORD]"
+echo "Migrating ..."
 
 function doUpload() {
-
   if [[ ${1#$REPOSITORY_PATH} != *'_remote.repositories'* ]]; then
-    curl -v -u $USERNAME:$PASSWORD -T $1 "${REMOTE_REPO_URL}${1#$REPOSITORY_PATH}"
+    echo "============================ Start =================================="
+    echo "curl -i -u $CODING_USERNAME:$CODING_PASSWORD -T $1 '${REMOTE_REPO_URL}${1#$REPOSITORY_PATH}'"
+    curl -i -u $CODING_USERNAME:$CODING_PASSWORD -T $1 "${REMOTE_REPO_URL}${1#$REPOSITORY_PATH}"
+    echo "Migrated $1"
+    echo "============================  End  =================================="
   fi
 }
 
